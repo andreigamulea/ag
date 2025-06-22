0fda4fc626bd545836b77776ef146d10b8f893b2
andreigamulea <ayushcellromania@gmail.com>
2025-06-22 18:09:37 +0300

    v1

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 set -o errexit
-echo "🧹 Curăț cache vechi"
-rm -rf tmp/cache
-bundle install
-yarn install
 
+echo "🧹 Curățare cache vechi..."
+rm -rf tmp/cache
+
+echo "📦 Instalez gem-uri..."
+bundle install
+
+echo "📦 Instalez pachete JS..."
+yarn install --frozen-lockfile
+
+echo "🎨 Compilez Tailwind + CSS personalizat..."
 yarn tailwindcss -c tailwind.config.js -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css --minify
 
+echo "🧱 Precompilez assets..."
 bundle exec rake assets:precompile
+
+echo "🗄 Migrez baza de date..."
 bundle exec rake db:migrate
+
+echo "✅ Build finalizat cu succes!"
